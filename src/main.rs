91a40ce30e2889c48e02c0d2b14fb3f7af5baefe
Chris Fahlin <chris_fahlin@comcast.net>
2016-12-12 11:09:27 -0800

    Added comparison code to determine whether the guess was too big, too small or on point

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 extern crate rand;
 
 use std::io;
+use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
@@ -9,7 +10,7 @@ fn main() {
     let secret_number = rand::thread_rng().gen_range(1,101);
 
     println!("The secret number is {}", secret_number);
-    
+
     println!("Please input your guess.");
 
     let mut guess = String::new();
@@ -17,5 +18,13 @@ fn main() {
     io::stdin().read_line(&mut guess)
         .expect("Failed to read line");
 
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
     println!("You guessed {}", guess);
+
+    match guess.cmp(&secret_number) {
+       Ordering::Less       => println!("Too Small!"),
+       Ordering::Greater    => println!("Too Big!"),
+       Ordering::Equal      => println!("You Win!"),
+    }
 }
